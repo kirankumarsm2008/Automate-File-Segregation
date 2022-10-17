a65a92761c1c232443d66afffbc2a944bd7e4312
kirankumarsm2008 <88017922+kirankumarsm2008@users.noreply.github.com>
2022-10-17 19:37:47 +0530

    Add files via upload

---
+++ Move_Fily.py
@@ -0,0 +1,13 @@
+import os 
+import shutil
+
+from_dir = 'K:/'
+to_dir = 'Documents/'
+
+list_of_files = os.listdir(from_dir)
+print(list_of_files)
+
+for k in list_of_files:
+    os.path.splitext()
+
+    
